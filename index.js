0f7bdd3fc3d956cd898c0a2a07016bb1c1fe55ff
lourdes-dorvilus <lourdesdorvilus@yahoo.com>
2023-09-08 16:30:34 -0400

    set up functionality to add users to employee table

--- index.js
+++ index.js
@@ -1,56 +1,102 @@
-const inquirer = require('inquirer');
-const db = require("./config/connection")
+const inquirer = require("inquirer");
+const db = require("./config/connection");
 
 db.connect(function (err) {
-    if (err) throw err
-    console.log("MySQL Connected")
-    mainMenu()
+  if (err) throw err;
+  console.log("MySQL Connected");
+  mainMenu();
 });
 
 const questions = [
-    {
-        type: "list",
-        name: "questions",
-        message: "What do you want to do?",
-        choices: ["View All Employees", "View All Departments", "View All Roles", "Add Employee", "Add Department", "Add Role", "Update Employee Role"]
-    }
-]
+  {
+    type: "list",
+    name: "questions",
+    message: "What do you want to do?",
+    choices: [
+      "View All Employees",
+      "View All Departments",
+      "View All Roles",
+      "Add Employee",
+      "Add Department",
+      "Add Role",
+      "Update Employee Role",
+    ],
+  },
+];
 
 const mainMenu = () => {
-    inquirer.prompt(questions)
-    .then((res) => {
-        if(res.questions === "View All Employees") {
-            viewAllEmployees()
-        }
-        if(res.questions === "View All Departments") {
-            viewAllDepartments()
-        }
-        if(res.questions === "View All Roles") {
-            viewAllRoles()
-        }
-    })
-}
+  inquirer.prompt(questions).then((res) => {
+    if (res.questions === "View All Employees") {
+      viewAllEmployees();
+    }
+    if (res.questions === "View All Departments") {
+      viewAllDepartments();
+    }
+    if (res.questions === "View All Roles") {
+      viewAllRoles();
+    }
+    if (res.questions === "Add Employee") {
+      addEmployee();
+    }
+  });
+};
 
 const viewAllDepartments = () => {
-    db.query("SELECT * FROM department", function(err, res) {
-        if (err) throw err;
-        console.table(res)
-        mainMenu()
-    })
-}
+  db.query("SELECT * FROM department", function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    mainMenu();
+  });
+};
 
 const viewAllRoles = () => {
-    db.query("SELECT * FROM role", function(err, res) {
-        if (err) throw err;
-        console.table(res)
-        mainMenu()
-    })
-}
+  db.query("SELECT * FROM role", function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    mainMenu();
+  });
+};
 
 const viewAllEmployees = () => {
-    db.query("SELECT * FROM employee", function(err, res) {
-        if (err) throw err;
-        console.table(res)
-        mainMenu()
-    })
-}
+  db.query("SELECT * FROM employee", function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    mainMenu();
+  });
+};
+
+const addEmployee = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "firstName",
+        message: "What is this employee's first name?",
+      },
+      {
+        type: "input",
+        name: "lastName",
+        message: "What is this employee's last name?",
+      },
+      {
+        type: "input",
+        name: "role",
+        message: "What is this employee's role id?",
+      },
+      {
+        type: "input",
+        name: "manager",
+        message: "What is this employee's manager id?",
+      },
+    ])
+    .then((res) => {
+      db.query(`INSERT INTO employee SET ?`, {
+        first_name: res.firstName,
+        last_name: res.lastName,
+        role_id: res.role,
+        manager_id: res.manager,
+      });
+      console.log(`Employee was added`);
+      mainMenu();
+    });
+};
