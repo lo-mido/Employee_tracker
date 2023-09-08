2d7e1dba140ae9fa0c86c28ca4eee0b61b79d93e
lourdes-dorvilus <lourdesdorvilus@yahoo.com>
2023-09-08 16:24:46 -0400

    set up function to render all employees in the consle if user chooses to view all employees

--- index.js
+++ index.js
@@ -4,4 +4,31 @@ const db = require("./config/connection")
 db.connect(function (err) {
     if (err) throw err
     console.log("MySQL Connected")
+    mainMenu()
 });
+
+const questions = [
+    {
+        type: "list",
+        name: "questions",
+        message: "What do you want to do?",
+        choices: ["View All Employees", "View All Departments", "View All Roles", "Add Employee", "Add Department", "Add Role", "Update Employee Role"]
+    }
+]
+
+const mainMenu = () => {
+    inquirer.prompt(questions)
+    .then((res) => {
+        if(res.questions === "View All Employees") {
+            viewAllEmployees()
+        }
+    })
+}
+
+const viewAllEmployees = () => {
+    db.query("SELECT * FROM employee", function(err, res) {
+        if (err) throw err;
+        console.table(res)
+        mainMenu()
+    })
+}
